f4942506b71c723d36ebce1a8269599c83e5d62a
Anindya Sekar Paramitha <71497035+shamankares@users.noreply.github.com>
2022-06-15 21:37:09 +0700

    Melanjutkan penulisan kode dari algoritma A* (WIP)

--- SmartCourrier_init.py
+++ SmartCourrier_init.py
@@ -14,6 +14,9 @@ class Node():
         self.f, self.g, self.h = 0
         self.parent = parent
         self.position = None
+    
+    def __eq__(self, other):
+        return self.position == other.position
 
 class Courrier(ttl.Turtle):
     def __init__(self, direction=Direction.EAST, position=None):
@@ -107,10 +110,7 @@ def searchDestination(maze, start, destination):
         close_list = []
 
         start_node = Node(None, start)
-        end_node = Node(None, destination.position)
-        end_area_nodes = []
-        for pos in destination.area:
-            end_area_nodes.append(Node(None, pos))
+        end_node = Node(None, destination)
 
         open_list.append(start)
 
@@ -131,7 +131,7 @@ def searchDestination(maze, start, destination):
             # Jika node terbaik adalah node sekitar end, kembalikan
             # jalur dari node tersebut
 
-            if best_node in end_area_nodes:
+            if best_node == end_node:
                 path = []
                 node = best_node
                 while node is not None:
@@ -141,6 +141,38 @@ def searchDestination(maze, start, destination):
                 return path
 
             # Jika bukan, maka buat penerus dari node terbaik
+            successors = []
+            for new_position in [(1, 0), (0, 1), (0, -1), (-1, 0)]:
+                node_position = (best_node.position[0] + new_position[0], best_node.position[1] + new_position[1])
+                
+                if node_position[0] > (len(maze) - 1) or node_position[1] > (len(maze) - 1):
+                    continue
+                if node_position[0] < 0 or node_position[1] < 0:
+                    continue
+                
+                if maze[node_position[0]][node_position[1]] != 0:
+                    continue
+                
+                new_node = Node(best_node, node_position)
+                successors.append(new_node)
+
+            for cadidate in successors:
+                cadidate.g = best_node.g + 1
+                cadidate.h = math.fabs(cadidate.position[0] - end_node.position[0]) + math.fabs(cadidate.position[1] - end_node.position[1])    # Manhattan distance heuiristics
+                cadidate.f = cadidate.g + cadidate.h
+
+                for open_node in open_list:
+                    if cadidate == open_node:
+                        successors.append(open_node)
+                        if open_node.g < cadidate.g:
+                            open_node.parent = best_node
+                            open_node.g = best_node.g + 1
+                            open_node.f = open_node.g + open_node.h
+                
+                for close_node in close_list:
+                    if cadidate.position == close_node.position:
+                        successors.append(close_node)
+                        #if
 
             pass
         
@@ -183,10 +215,10 @@ def main():
     # 2. Buat list jalur dan maze
     jalur = []
                 #0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 
-    maze = [    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], # 0
-                [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1], # 1
-                [1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1], # 2
-                [1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1], # 3
+    maze = [    [0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1], # 0
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], # 1
+                [0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0], # 2
+                [0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1], # 3
                 [1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1], # 4
                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1], # 5
                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], # 6
@@ -195,9 +227,9 @@ def main():
                 [1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1], # 9
                 [1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1], # 10
                 [1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1], # 11
-                [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1], # 12
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], # 13
-                [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], # 14
+                [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0], # 12
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], # 13
+                [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1], # 14
             ]
     drawMaze(maze)
     start_pos = placeStart(maze)
